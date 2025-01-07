c7c3b5dcaf7b67dc7dc6b227711270be07541d42
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-01-07 15:22:06 +0530

    excersie

--- app/src/main/java/com/example/ampproject/homepage.java
+++ app/src/main/java/com/example/ampproject/homepage.java
@@ -1,6 +1,9 @@
 package com.example.ampproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class homepage extends AppCompatActivity {
 
+  LinearLayout card1, card2, card3;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_homepage);
+
+    card1 = findViewById(R.id.card1);
+    card2 = findViewById(R.id.card2);
+    card3 = findViewById(R.id.card3);
+
+    card1.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(homepage.this,Under18.class);
+        startActivity(intent);
+        finish();
+      }
+    });
   }
 }
